034672b8f0724c92f5a7a6f43a4e58746ac7945d
Christian Prado <chris.a.prado@gmail.com>
2023-06-29 19:03:08 -0700

    Setup the frontend with Vite, TS, & MUI

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { Box, Typography } from '@mui/material'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <Box>
+      <Typography variant="h1" component="div" gutterBottom>
+        Vite + React
+      </Typography>
+      <Typography variant='body1'>
+        Click on the Vite and React logos to learn more
+      </Typography>
+    </Box>
+  )
+}
+
+export default App
